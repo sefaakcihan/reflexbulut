798c0b01aecf6da0b237396d8cdc538654f5561c
FerhatKLNC <cutewt43@gmail.com>
2023-10-03 10:11:19 +0300

    düzenlemeyapıldı
    ff

--- app/Views/users/index.php
+++ app/Views/users/index.php
@@ -42,18 +42,7 @@ helper(["yardimcifonksiyonlar"]);
                     </div>
                     <div class="ms-auto">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-outline-primary">Settings</button>
-                            <button type="button"
-                                class="btn btn-outline-primary split-bg-primary dropdown-toggle dropdown-toggle-split"
-                                data-bs-toggle="dropdown"> <span class="visually-hidden">Toggle Dropdown</span>
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg-end"> <a
-                                    class="dropdown-item" href="javascript:;">Action</a>
-                                <a class="dropdown-item" href="javascript:;">Another action</a>
-                                <a class="dropdown-item" href="javascript:;">Something else here</a>
-                                <div class="dropdown-divider"></div> <a class="dropdown-item"
-                                    href="javascript:;">Separated link</a>
-                            </div>
+                            <button type="button" class="btn btn-outline-primary">Kullanıcı Ekle</button>   
                         </div>
                     </div>
                 </div>
